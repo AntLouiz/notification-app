51dd701fa7bbd1985aa4c67ab14c9a1a6d3267a4
AntLouiz <luizrodrigo46@hotmail.com>
2020-04-29 10:55:55 -0300

    Add first implementation of db create

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
 class Notification {
 
   String title;
@@ -6,4 +11,26 @@ class Notification {
   bool isChecked = false;
 
   Notification({this.title, this.body, this.isActive, this.isChecked});
+}
+
+
+class NotificationDB {
+  Future<Database> database;
+
+  Future<void> open() async {
+    this.database = openDatabase(
+      join(await getDatabasesPath(), 'notifications.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE notifications("
+          "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+          "title TEXT NOT NULL,"
+          "body TEXT NOT NULL,"
+          "isActive BOOLEAN DEFAULT 1,"
+          "isChecked BOOLEAN DEFAULT 0"
+        );
+      },
+      version: 1
+    );
+  }
 }
